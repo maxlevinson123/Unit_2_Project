827216c8599c0ad3e5f6211c23658d143bcde10a
MaxLevinson <maxl77@nycstudents.net>
2025-11-19 08:42:11 -0500

    a

--- src/Game.java
+++ src/Game.java
@@ -2,4 +2,12 @@ public class Game {
     public static int rollDice() {
         return (int) (Math.random() * 6 + 1);
     }
+
+    public static String askQuestion() {
+        int a = Game.rollDice();
+        if (a == 1) {
+            return "What is 4 x 6?: ";
+        }
+        return "";
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println(Game.askQuestion());
+
+
         System.out.println("Welcome to the Awesome Game!");
         System.out.print("How many players would you like to play with?: ");
         int players = scan.nextInt();
+
+        int score = 0;
+        while (score < 100) {
+            score+= Game.rollDice();
+
+        }
+
     }
 }
+
