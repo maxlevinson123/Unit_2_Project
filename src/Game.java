4dd2060ba3e0afb8e630f4d8649ea07d31850184
MaxLevinson <maxl77@nycstudents.net>
2025-11-20 08:46:23 -0500

    a

--- src/Game.java
+++ src/Game.java
@@ -2,12 +2,5 @@ public class Game {
     public static int rollDice() {
         return (int) (Math.random() * 6 + 1);
     }
-
-    public static String askQuestion() {
-        int a = Game.rollDice();
-        if (a == 1) {
-            return "What is 4 x 6?: ";
-        }
-        return "";
-    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println(Game.askQuestion());
 
 
         System.out.println("Welcome to the Awesome Game!");
@@ -11,13 +10,24 @@ public class Main {
 
         int score = 0;
         while (score < 100) {
-            score+= Game.rollDice();
-            if (score % 10 == 0) {
-                Game.askQuestion();
+            score+= 4; // Game.rollDice();
+            if ((score == 4) || (score == 32) || (score == 58)) {
+                System.out.println("What is 4 x 6?: ");
+                String answer = scan.nextLine();
+                if (answer.equals("24")) {
+                    System.out.println("That's correct you get 5 points! " + score + " + 5:");
+                    score += 5;
+
+                }
+                else {
+                    System.out.println("That's wrong! No points for you!");
+                }
             }
             if (score % 7 == 0) {
                 score -= 5;
             }
+            System.out.println(score);
+
 
         }
 
