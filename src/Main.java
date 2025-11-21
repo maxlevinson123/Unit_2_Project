1d368f0d252fd60730a60cf4df28aa8f25f61c3e
MaxLevinson <maxl77@nycstudents.net>
2025-11-21 08:46:21 -0500

    a

--- src/Main.java
+++ src/Main.java
@@ -8,13 +8,36 @@ public class Main {
         System.out.print("How many players would you like to play with?: ");
         int players = scan.nextInt();
 
+        String answer = "";
         int score = 0;
         while (score < 100) {
             score+= 4; // Game.rollDice();
             if ((score == 4) || (score == 32) || (score == 58)) {
                 System.out.println("What is 4 x 6?: ");
-                String answer = scan.nextLine();
+                answer = scan.nextLine();
                 if (answer.equals("24")) {
+                    System.out.println("That's correct you get 5 points! " + score + " + 5:");
+                    score += 5;
+
+                }
+                else {
+                    System.out.println("That's wrong! No points for you!");
+                }
+            if ((score == 4) || (score == 32) || (score == 58)) {
+                System.out.println("What is 4 x 6?: ");
+                answer = scan.nextLine();
+                if (answer.equals("24")) {
+                    System.out.println("That's correct you get 5 points! " + score + " + 5:");
+                    score += 5;
+
+                }
+                else {
+                    System.out.println("That's wrong! No points for you!");
+                }
+            if ((score == 8) || (score == 69) || (score == 76)) {
+                System.out.println("The three primary colors are blue, yellow, and ______: (put answer in lowercase");
+                answer = scan.nextLine();
+                if (answer.equals("red")) {
                     System.out.println("That's correct you get 5 points! " + score + " + 5:");
                     score += 5;
 
