c3e62a74c6484a14981fd25745e7d7d6d0d408f8
MaxLevinson <maxl77@nycstudents.net>
2025-11-25 08:46:16 -0500

    a

--- src/Game.java
+++ src/Game.java
@@ -2,5 +2,16 @@ public class Game {
     public static int rollDice() {
         return (int) (Math.random() * 6 + 1);
     }
+
+    public static String askQuestion(int a) {
+        if (a == 1){
+            return "What is 4 x 6?: ";
+        }
+        return "";
+    }
+
+    public static String getAnswer(int a) {
+        return "";
+    }
 }
 
